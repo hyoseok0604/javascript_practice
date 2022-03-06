a8178d031baf61510592e4720394f4eabcb93c7a
hyoseok <janghs0604@gmail.com>
2022-03-07 02:58:46 +0900

    Add types/number.js

---
+++ types/number.js
@@ -0,0 +1,12 @@
+// number
+// number type : integer, floating point number
+
+let n1 = 1;
+let n2 = 1.5;
+
+// Infinity
+let inf1 = Infinity;
+let inf2 = 1 / 0; // divided by zero is Infinity
+
+// NaN
+let nan = "hello world" / 2;
